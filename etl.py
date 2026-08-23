db699c892aa89c002347c75fe2a0de8e2868b1f8
Illia263 <matsykur.illya@gmail.com>
2026-08-24 01:11:04 +0200

    task completed

--- etl.py
+++ etl.py
@@ -4,177 +4,144 @@
 """
 
 import csv
-import os
 from datetime import datetime
 from decimal import Decimal
-import argparse
-import psycopg2
 import requests
 import json
-import sys
-import structlog
-import uuid
-from structlog.contextvars import bind_contextvars, clear_contextvars
 from tenacity import retry, wait_random_exponential, retry_if_exception_type, stop_after_attempt
 import tempfile
+import pendulum
+from airflow.sdk import ObjectStoragePath, dag, task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+import logging
 
 
-DB = psycopg2.connect("dbname=taxi user=postgres password=postgres host=localhost")
+logger = logging.getLogger(__name__)
 
-log_file = open("etl.log", "a", encoding='utf-8') 
-structlog.configure(
-    processors=[
-        structlog.contextvars.merge_contextvars,
-        structlog.processors.add_log_level,
-        structlog.processors.TimeStamper(fmt="iso"),
-        structlog.processors.JSONRenderer()
-        
-
-    ],
-   logger_factory=structlog.WriteLoggerFactory(file=log_file)
+@dag(
+    dag_id="taxi_data",
+    schedule="@monthly",
+    start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
+    end_date=pendulum.datetime(2024,12,31, tz="UTC"),
+    catchup=False,
+    tags=["taxi", "2024"]
 )
-logger = structlog.get_logger()
-
-@retry(wait=wait_random_exponential(multiplier=1, max=60), stop=stop_after_attempt(3), retry=retry_if_exception_type((requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.HTTPError)), reraise=True)
-def download(month, dest="/tmp/data"):
-    logger.info('download_started', month=month, destination = dest)
-    url = f"http://localhost:8000/yellow_tripdata_{month}.csv"
-    r = requests.get(url)
-    r.raise_for_status()
-    os.makedirs(dest, exist_ok=True)
-    path = os.path.join(dest, f"{month}.csv")
-    open(path, "wb").write(r.content)
-    return path
-
-
-def load(path, target_month):
-    logger.info("load_started", path=path)
-    error_count = 0
-    total_rows = 0
-    def rows_parsing(rows):
-        nonlocal total_rows
-        for r in rows:
-            
-            total_rows += 1
-            if not r["tpep_pickup_datetime"].startswith(target_month):
-                yield("bad", {'raw-data' : r, 'reason' : 'This date was not requested'})
-                continue
-            try:
-                parsed = {
-                    "vendor": int(r["VendorID"]),
-                    "pickup": datetime.strptime(r["tpep_pickup_datetime"], "%Y-%m-%d %H:%M:%S"),
-                    "dist": float(r["trip_distance"]),
-                    "total": Decimal(r["total_amount"]),
-                }
-                yield("good", parsed)
-                
-
-            except Exception as e:
-                logger.warning("row_parsing_failed", reason=str(e), vendor_id = r.get("VendorID"))
-                yield("bad", {'raw-data' : r, 'reason' : str(e)})
-
-
+def taxi_pipeline():
     
-    with open(path, 'r') as raw_f, open("dead-letter.ndjson", 'a', encoding='utf-8') as dead_letter, tempfile.TemporaryFile("w+", newline='',  encoding='utf8') as temp_csv:
-        rows = csv.DictReader(raw_f)
-        stream = rows_parsing(rows)
-        writer = csv.writer(temp_csv)
-        for status, item in stream:
-            if status == "good":
-                 writer.writerow([
-                      item['vendor'],
-                      item['pickup'],
-                      item['dist'],
-                      item['total']
-                 ])
+    @task()
+    @retry(wait=wait_random_exponential(multiplier=1, max=60), stop=stop_after_attempt(3), retry=retry_if_exception_type((requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.HTTPError)), reraise=True)
+    def download(target_month ):
+        url = f"http://localhost:8000/yellow_tripdata_{target_month}.csv"
+        r = requests.get(url)
+        r.raise_for_status()
+        base_dir = ObjectStoragePath("file:///tmp/taxi_data/")
+        file_path = base_dir / f"yellow_tripdata_{target_month}.csv"
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        with file_path.open("wb") as f:
+            f.write(r.content)
+        return file_path
+
+
+    @task()
+    def load(input_path, target_month):
+        hook = PostgresHook(postgres_conn_id="internship_airflow_pipeline")
+        DB = hook.get_conn()
+        logger.info(f"load_started, path={input_path}")
+        error_count = 0
+        total_rows = 0
+        def rows_parsing(rows):
+            nonlocal total_rows
+            for r in rows:
                 
+                total_rows += 1
+                if not r["tpep_pickup_datetime"].startswith(target_month):
+                    yield("bad", {'raw-data' : r, 'reason' : 'This date was not requested'})
+                    continue
+                try:
+                    parsed = {
+                        "vendor": int(r["VendorID"]),
+                        "pickup": datetime.strptime(r["tpep_pickup_datetime"], "%Y-%m-%d %H:%M:%S"),
+                        "dist": float(r["trip_distance"]),
+                        "total": Decimal(r["total_amount"]),
+                    }
+                    yield("good", parsed)
+                    
 
-            elif status == "bad":
-                error_count += 1
-                
-                json.dump(item, dead_letter, ensure_ascii=False)
-                dead_letter .write("\n")
-        if total_rows > 0:
-            temp_csv.seek(0)
+                except Exception as e:
+                    logger.warning(f"row_parsing_failed, reason={str(e)}, vendor_id = {r.get('VendorID')}")
+                    yield("bad", {'raw-data' : r, 'reason' : str(e)})
 
-            with DB.cursor() as cursor:
 
-                cursor.execute(""" CREATE TEMP TABLE temp_trips (
-                        vendor INTEGER,
-                        pickup TIMESTAMP,
-                        dist DOUBLE PRECISION, 
-                        total NUMERIC ) ON COMMIT DROP;
-                        """)
-                cursor.copy_expert( 
+        
+        with input_path.open('r', encoding='utf-8') as raw_f, open("dead-letter.ndjson", 'a', encoding='utf-8') as dead_letter, tempfile.TemporaryFile("w+", newline='',  encoding='utf8') as temp_csv:
+            rows = csv.DictReader(raw_f)
+            stream = rows_parsing(rows)
+            writer = csv.writer(temp_csv)
+            for status, item in stream:
+                if status == "good":
+                    writer.writerow([
+                        item['vendor'],
+                        item['pickup'],
+                        item['dist'],
+                        item['total']
+                    ])
+                    
+
+                elif status == "bad":
+                    error_count += 1
+                    
+                    json.dump(item, dead_letter, ensure_ascii=False)
+                    dead_letter .write("\n")
+            if total_rows > 0:
+                temp_csv.seek(0)
+
+                with DB.cursor() as cursor:
+
+                    cursor.execute(""" CREATE TEMP TABLE temp_trips (
+                            vendor INTEGER,
+                            pickup TIMESTAMP,
+                            dist DOUBLE PRECISION, 
+                            total NUMERIC ) ON COMMIT DROP;
+                            """)
+                    cursor.copy_expert( 
+                        """
+                            COPY temp_trips (vendor, pickup, dist, total)
+                            FROM STDIN WITH (FORMAT csv); 
+                            """, temp_csv) 
+                    
+                    cursor.execute(
+                    """ INSERT INTO trips(vendor, pickup, dist, total)
+                        SELECT vendor, pickup, dist, total
+                        FROM temp_trips
+                        ON CONFLICT (vendor, pickup, dist) DO NOTHING;
+                    
+                    
                     """
-                        COPY temp_trips (vendor, pickup, dist, total)
-                        FROM STDIN WITH (FORMAT csv); 
-                        """, temp_csv) 
-                
-                cursor.execute(
-                """ INSERT INTO trips(vendor, pickup, dist, total)
-                    SELECT vendor, pickup, dist, total
-                    FROM temp_trips
-                    ON CONFLICT (vendor, pickup, dist) DO NOTHING;
-                
-                
-                """
-                
-                
-            )
+                    
+                    
+                )
 
 
 
-    
-    if total_rows ==0:
-        return 0
         
-
-    error_rate = error_count / total_rows
-    if error_rate > 0.05:
-        logger.error("More than 5'%' of broken rows", total_loaded = total_rows - error_count, error_count=error_count)
-        DB.rollback()
-        sys.exit(1)
-        
+        if total_rows ==0:
+            return 0
             
-    DB.commit()
-    return total_rows - error_count
 
-
-def run():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--month", type=str)
-    parser.add_argument("--backfill",type=str)
-    data = parser.parse_args()
-    clear_contextvars()
-    run_id = str(uuid.uuid4())
-    bind_contextvars(run_id=run_id)
-    logger.info("pipeline_started", target_month = data.month)
-    try:
-        if data.month:
-
-            path = download(data.month)
-            loaded_rows_count = load(path, data.month)
-            logger.info("Pipeline_completed_successfully", final_rows=loaded_rows_count)
-        elif data.backfill:
-            start_month, end_month = data.backfill.split(":") #2024-01:202412
-            year = start_month.split("-")[0]
-            start_mnth = int(start_month.split("-")[1])
-            end_mnth = int(end_month.split("-")[1])
-            for m in range (start_mnth, end_mnth+1):
-                current_month = f"{year}-{m:02d}"
-                path = download(current_month)
-                loaded_rows_count = load(path, current_month)
-                logger.info("Pipeline_completed_successfully", final_rows=loaded_rows_count)
+        error_rate = error_count / total_rows
+        if error_rate > 0.05:
+            logger.error(f"More than 5'%' of broken rows, total_loaded = {total_rows - error_count}, error_count={error_count}")
+            DB.rollback()
+            raise RuntimeError("More than 5'%' of broken rows")
             
-        else:
-            logger.error("No --month or --backfill detected")
-            sys.exit(2)
-    except (requests.exceptions.RequestException, psycopg2.Error) as e:
-        logger.exception('infrastructure error', error=str(e) )
-        sys.exit(2)
+                
+        DB.commit()
+        return total_rows - error_count
+    current_month = "{{data_interval_start.format('YYYY-MM')}}" 
+    downloaded_file = download(target_month=current_month)
+    load(input_path=downloaded_file, target_month=current_month)
 
 
 
-if __name__ == "__main__":
-    run()
+
+taxi_pipeline()
